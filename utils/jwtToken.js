3d1aec9f23857da56467aedfbf0ab635540c4590
kandika-Rohan <rohankandika05@gmail.com>
2025-03-13 15:56:13 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -16,7 +16,7 @@
 // };
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
-  console.log("Generated JWT Token:", token);
+  
 
   const options = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
